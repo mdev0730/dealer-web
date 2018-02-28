d1a98a1c6e3b047b949413d3c0ef003580d07b41
cheera <hashming0730@yahoo.com>
2018-02-28 16:17:36 +1000

    fixed request view

--- src/request/index.js
+++ src/request/index.js
@@ -207,19 +207,21 @@ class Request extends Component {
         <div style={{ width: '100%' }}>
           <div className="request-content">
             <div className="request-content-header">
-              <img className="image" src={require('../shared/img/group.png')} />
-              <div className="groupname">
-                <div className="group">TOGEL Constractors</div>
-                <div className="project">Project Name: Gypsum Works</div>
-              </div>
-              <div className="delivery">
-                <div><b>Delivery date:</b>21/02/18</div>
-                <div><b>Address:</b> 3 Hanoshoshet St. Tel Aviv</div>
-              </div>
-              <div className="payment">
-                <div><b>Payment:</b> By credit card</div>
-                <div><b>Remark:</b> Half payment on the card and the rest on checks every month</div>
-              </div>
+              {/* <div> */}
+                <img className="image" src={require('../shared/img/group.png')} />
+                <div className="groupname">
+                  <div className="group">TOGEL Constractors</div>
+                  <div className="project">Project Name: Gypsum Works</div>
+                </div>
+                <div className="delivery">
+                  <div><b>Delivery date:</b>21/02/18</div>
+                  <div><b>Address:</b> 3 Hanoshoshet St. Tel Aviv</div>
+                </div>
+                <div className="payment">
+                  <div><b>Payment:</b> By credit card</div>
+                  <div><b>Remark:</b> Half payment on the card and the rest on checks every month</div>
+                </div>
+              {/* </div> */}
             </div>
             <div className="colors-paint">
               <div className="data">Colors and paint</div>
