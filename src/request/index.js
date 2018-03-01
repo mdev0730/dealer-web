2a98c1af569c64b16133929844b93c4ed584c49a
cheera <hashming0730@yahoo.com>
2018-03-01 17:09:00 +1000

    send success modal added

--- src/request/index.js
+++ src/request/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Breadcrumb, Table, Button, Icon } from 'antd';
+import { Breadcrumb, Table, Button, Icon, Modal } from 'antd';
 import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 import SearchInput, { createFilter } from 'react-search-input'
@@ -176,13 +176,27 @@ class Request extends Component {
   replaceRequest() {
 
   }
-
+  handleOk = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  }
   searchUpdated(term) {
     //   alert(term);
     // this.setState({searchTerm: term})
   }
+  sendClick = () => {
+    this.setState({
+      visible: true,
+    });
+  }
   render() {
-    const { companies, sellitems } = this.state;
+    const { companies, sellitems, visible } = this.state;
     const { fetchPlaces: { loading, allPlaces } } = this.props;
     if (loading) {
       return <div className="loader-indicator" />;
@@ -207,19 +221,19 @@ class Request extends Component {
         <div style={{ width: '100%' }}>
           <div className="request-content">
             <div className="request-content-header">
-                <img className="image" src={require('../shared/img/group.png')} />
-                <div className="groupname">
-                  <div className="group">TOGEL Constractors</div>
-                  <div className="project">Project Name: Gypsum Works</div>
-                </div>
-                <div className="delivery">
-                  <div><b>Delivery date:</b>21/02/18</div>
-                  <div><b>Address:</b> 3 Hanoshoshet St. Tel Aviv</div>
-                </div>
-                <div className="payment">
-                  <div><b>Payment:</b> By credit card</div>
-                  <div><b>Remark:</b> Half payment on the card and the rest on checks every month</div>
-                </div>
+              <img className="image" src={require('../shared/img/group.png')} />
+              <div className="groupname">
+                <div className="group">TOGEL Constractors</div>
+                <div className="project">Project Name: Gypsum Works</div>
+              </div>
+              <div className="delivery">
+                <div><b>Delivery date:</b>21/02/18</div>
+                <div><b>Address:</b> 3 Hanoshoshet St. Tel Aviv</div>
+              </div>
+              <div className="payment">
+                <div><b>Payment:</b> By credit card</div>
+                <div><b>Remark:</b> Half payment on the card and the rest on checks every month</div>
+              </div>
             </div>
             <div className="colors-paint">
               <div className="data">Colors and paint</div>
@@ -257,13 +271,26 @@ class Request extends Component {
               </div>
             </div>
           </div>
-          <div className="send-btt" id="sendbutton">
+          <div className="send-btt" id="sendbutton" onClick={() => this.sendClick()} >
             <div style={{ display: 'flex' }} >
               Send
             <img src={require('../shared/img/check1.png')} />
             </div>
           </div>
         </div>
+        <Modal itle=""
+          visible={visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          className="modal-send"
+        >
+          <div className="send-success-dialog">
+            <img src={require('../shared/img/success.png')} className="success-img" />
+            <div className="success-txt">Your price quote</div>
+            <div className="success-txt2">was sent!</div>
+            <Button className="success-ok" onClick={this.handleOk}>ok</Button>
+          </div>
+        </Modal>
       </div>
     )
   }
